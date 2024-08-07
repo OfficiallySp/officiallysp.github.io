1faf779081a8b5f9709853b7c86b820a30b3f8c1
OfficiallySp <shane.pepperell@me.com>
2024-08-07 03:02:41 +0100

    new pages and stuff. still a wip

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('navbar.html')
+    // Get the current path
+    const currentPath = window.location.pathname;
+    
+    // Calculate the relative path to the root
+    const pathToRoot = currentPath.split('/').slice(0, -1).map(() => '..').join('/') || '.';
+    
+    // Construct the path to navbar.html
+    const navbarPath = `${pathToRoot}/navbar.html`;
+
+    fetch(navbarPath)
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
